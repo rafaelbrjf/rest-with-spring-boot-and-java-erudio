fe0690f57d6b98c26b2d2acf7f60060c4a6d9e6b
Rafael Braga <dutra.rafaelbraga@gmail.com>
2024-08-15 23:32:16 -0300

    First Steps in Java with Spring Boot 3

---
+++ src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,24 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
